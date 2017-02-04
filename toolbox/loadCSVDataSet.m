a8f7e8d62cb62064b9a5520fe884fc50ee0e2ccd
U-Rain-Desktop\Tom <twgr@robots.ox.ac.uk>
2017-02-04 22:36:49 +0000

    Catch missing values in loadCSVDataSet function and allow for named features in csv file

--- toolbox/loadCSVDataSet.m
+++ toolbox/loadCSVDataSet.m
@@ -1,30 +1,50 @@
-function [X,Y,bOrdinal] = loadCSVDataSet(inputLocation)
+function [X,Y,bOrdinal] = loadCSVDataSet(inputLocation,bNamed)
 %loadCSVDataSet Loads a formatted csv to give features and classes
 %
-% funtion [X,Y,bOrdinal] = loadCSVDataSet(inputLocation)
+% function [X,Y,bOrdinal] = loadCSVDataSet(inputLocation)
 %
-% inputLocation = String giving location of an input csv file.  The
-%     last column gives the class labels with featuers in the other 
-%     columns.  The following delimiters are supported: 
-%               [' ' ',' '|' ';' tab].  
-%     Classes can either be numerical or strings.  If the dataset contains 
-%     non-ordinal features then these should either be specified using 
-%     non-numeric symbols or by specifying a header line of the form 
-%               bOrdinal%1%1%0%1%... 
-%     where % indicates the delimiter used, 1 indicates the respective 
-%     feature is ordinal and 0 that it is an unordered categorical.  If
-%     bOrdinal is not specified, all features are assumed to be ordinal.
-%     For an example of these see the processed datasets in Dataset folder 
+% Loads a CSV to produce X, Y and bOrdinal.  The CSV should be formatted as
+% follows:
+%     The last column gives the target Y with featuers X in the other
+%     columns.  The following delimiters are supported:
+%               [' ' ',' '|' ';' tab].
+%     Classes can either be numerical or strings.  If the dataset contains
+%     non-ordinal features then these should either be specified using
+%     non-numeric symbols or by specifying a header line of the form
+%               bOrdinal%1%1%0%1%...
+%     where % indicates the delimiter used, 1 indicates the respective
+%     feature is ordinal and 0 that it is an unordered categorical.
+%     For an example of these see the processed datasets in Dataset folder
 %     of the directory (soybean gives an example where bOrdinal is used).
+%     If bOrdinal is not specified then variables are assumed to be ordinal
+%     unless the variable has MORE than one unique non-numeric input.  If
+%     there is only one non-numeric input this is assumed to correspond to
+%     a missing value.  For example if a feature has values 1 2 ? 4 ? it
+%     will be converted to an ordinal feature with corresponding x values
+%     1 2 NaN 4 NaN, but 1 2 ? 4 a will be treated as a non-ordinal feature
+%     with values '1' '2' '?' '4' 'a'.
+%
+% Inputs: inputLocation = String giving location of an input csv file.
+%         bNamed = Whether the csv contains names for the variables.  If so
+%                  these should come after the bOrdinal header line (if
+%                  this exists).  False by default
+%     
 %
 % Outpus:   X = Features, this will be a numeric array if possible and
-%               otherwise a cell array
+%               otherwise a cell array.  When a cell array, cell values
+%               will be numeric if all non-missing values of that feature
+%               are numeric, otherwise all values in column will be
+%               strings.
 %           Y = Class labels returned as a a numeric column vector when
 %               possible and a column vector of cells otherwise.
 %    bOrdinal = Row vector of logicals indicating whether a feature is
 %               ordinal (true) or an unordered categorical (false).
 %
 % Tom Rainforth 04/08/15
+
+if ~exist('bNamed','var') || isempty(bNamed)
+    bNamed = false;
+end
 
 fid = fopen(inputLocation,'r');
 firstLine = fgets(fid);
@@ -34,7 +54,7 @@ if strcmpi(firstLine(1:8),'bOrdinal')
     delimiter = firstLine(9:(iNum(1)-1));
     bOrdinal = arrayfun(@(x) logical(str2double(firstLine(x))), iNum);
     fclose(fid);
-    InputTable = readtable(inputLocation,'HeaderLines',1,'ReadVariableNames',false,'Delimiter',delimiter);
+    InputTable = readtable(inputLocation,'HeaderLines',1,'ReadVariableNames',bNamed,'Delimiter',delimiter);
     try
         X = table2array(InputTable(:,1:end-1));
     catch
@@ -52,16 +72,31 @@ else
         error('Invalid delimiter');
     end
     delimiter = firstLine(iDel(1));
-    InputTable = readtable(inputLocation,'ReadVariableNames',false,'Delimiter',delimiter);
+    InputTable = readtable(inputLocation,'ReadVariableNames',bNamed,'Delimiter',delimiter);
     try
         X = table2array(InputTable(:,1:end-1));
+        bOrdinal = true(1,size(X,2));
     catch
         X = table2cell(InputTable(:,1:end-1));
+        bNumeric = cellfun(@isnumeric,X);
+        bOrdinal = sum(~bNumeric,1)==0;
+        iContainsString = find(~bOrdinal);
+        for n=1:numel(iContainsString)
+            Xnum = cellfun(@str2double,X(~bNumeric(:,iContainsString(n)),iContainsString(n)));
+            bNotNum = isnan(Xnum);            
+            nUniqueStrings = numel(unique(X(bNotNum,iContainsString(n))));
+            if nUniqueStrings<=1
+                X(~bNumeric(:,iContainsString(n)),iContainsString(n)) = num2cell(Xnum);
+                bOrdinal(iContainsString(n)) = true;
+            end
+        end
+        if all(bOrdinal)
+            X = cell2mat(X);
+        end
     end
     try
         Y = table2array(InputTable(:,end));
     catch
         Y = table2cell(InputTable(:,end));
     end
-    bOrdinal = true(1,size(X,2));
 end
